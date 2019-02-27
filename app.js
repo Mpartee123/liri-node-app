8f9ece7985e868236021d2100a9b9cc3ceb5ed28
mpartee123 <m_partee@hotmail.com>
2019-02-26 18:20:08 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// https://www.npmjs.com/package/node-spotify-api
+// https://www.npmjs.com/package/axios
+// http://www.omdbapi.com
+// http://www.artists.bandsintown.com/bandsintown-api
+// https://www.npmjs.com/package/moment
+// https://www.npmjs.com/package/dotenv
