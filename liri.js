7d484bfa43fa70c516d9546e4046728e3273afdc
mpartee123 <m_partee@hotmail.com>
2019-03-02 17:21:59 -0600

    Spotify api call working

--- liri.js
+++ liri.js
@@ -9,12 +9,27 @@
 //
 //     * `concert-this`
 //
-// * `spotify-this-song`
-//
 // * `movie-this`
 //
 // * `do-what-it-says`
 
+
+
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+
 var spotify = new Spotify(keys.spotify);
+
+spotify.search({type: 'track', query: 'All the Small Things'}, function (err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+    var dataLoc = data.tracks.items[0];
+    console.log(`
+    Name: ${dataLoc.artists[0].name.toUpperCase()}
+    Track: ${dataLoc.name}
+    Album: ${dataLoc.album.name}
+    Link: ${dataLoc.href}
+    `);
+});
